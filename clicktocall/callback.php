53e022bf4e0c8921dff7cd1c74944eeefbec7953
joohoun song <joohoun@gmail.com>
2015-09-06 15:14:39 +0900

    Update callback.php

--- clicktocall/callback.php
+++ clicktocall/callback.php
@@ -1,8 +1,8 @@
 <?php
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  header("content-type: text/xml");
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say language="ja-jp">画面の転送先電話番号に電話をかけます。</Say>
-    <Dial callerId="+8150xxxxyyyy"><?php echo $_REQUEST['number']?></Dial>
+  <Say language="ja-jp">画面の転送先電話番号に電話をかけます。</Say>
+  <Dial callerId="+8150xxxxyyyy"><?php echo $_REQUEST['number']?></Dial>
 </Response>
