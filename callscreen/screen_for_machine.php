ace85b74af8c0625ad06f6e8f366bf495b4f7807
joohoun song <joohoun@gmail.com>
2015-09-06 15:36:59 +0900

    Update screen_for_machine.php

--- callscreen/screen_for_machine.php
+++ callscreen/screen_for_machine.php
@@ -1,9 +1,9 @@
 <?php header("content-type: text/xml");
-echo '<?xml version="1.0" encoding="UTF-8"?>';
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <Response>
-	<Gather action="complete_call.php">
-		<Say>Press any key to accept this call</Say>
-	</Gather>
-	<Hangup/>
+  <Gather action="complete_call.php">
+    <Say>Press any key to accept this call</Say>
+  </Gather>
+  <Hangup/>
 </Response>
