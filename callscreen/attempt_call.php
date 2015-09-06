b0855b70f1f8cf45b4a087b0dc462440eaa451f7
joohoun song <joohoun@gmail.com>
2015-09-06 15:35:38 +0900

    Update attempt_call.php

--- callscreen/attempt_call.php
+++ callscreen/attempt_call.php
@@ -1,30 +1,25 @@
 <?php 
+  $numbers = array("+8180xxxxxxxx", "+8180xxxxxxxx", "+8180xxxxxxxx");
+  $number_index = isset($_REQUEST['number_index']) ? $_REQUEST['number_index'] : "0";
+  $DialCallStatus = isset($_REQUEST['DialCallStatus']) ? $_REQUEST['DialCallStatus'] : "";
 
-// Set the numbers to call
-$numbers = array("+8180xxxxxxxx", "+8180xxxxxxxx", "+8180xxxxxxxx");
-$number_index = isset($_REQUEST['number_index']) ? $_REQUEST['number_index'] : "0";
-$DialCallStatus = isset($_REQUEST['DialCallStatus']) ? $_REQUEST['DialCallStatus'] : "";
+  header("content-type: text/xml"); 
 
-header("content-type: text/xml"); 
-
-// Check the status of the call and 
-// that there is a valid number to call
-
-if($DialCallStatus!="completed" && $number_index<count($numbers)){ 
+  if($DialCallStatus!="completed" && $number_index<count($numbers)){
 ?>
-	<Response>
-	<Dial action="attempt_call.php?number_index=<?php echo $number_index+1 ?>">
-		<Number url="screen_for_machine.php">
-		<?php echo $numbers[$number_index] ?>
-		</Number>
-	</Dial>
-	</Response>
+    <Response>
+      <Dial action="attempt_call.php?number_index=<?php echo $number_index+1 ?>">
+        <Number url="screen_for_machine.php">
+          <?php echo $numbers[$number_index] ?>
+        </Number>
+      </Dial>
+    </Response>
 <?php
-} else {
+  }else{
 ?>
-	<Response>
-		<Hangup/>
-	</Response>
+    <Response>
+      <Hangup/>
+    </Response>
 <?php
-}
+  }
 ?>
