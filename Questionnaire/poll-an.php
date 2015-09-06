0baa515772a4ee0483fd852a30305b8549f5c312
joohoun song <joohoun@gmail.com>
2015-09-06 15:18:37 +0900

    Update poll-an.php

--- Questionnaire/poll-an.php
+++ Questionnaire/poll-an.php
@@ -1,17 +1,17 @@
 <?php
-	require 'Services/Twilio.php';
+  require 'Services/Twilio.php';
 	
-	$response = new Services_Twilio_Twiml();
-	$pollTelNo = $_POST['From'];
+  $response = new Services_Twilio_Twiml();
+  $pollTelNo = $_POST['From'];
 	
-	$gather = $response->gather(array(
-		'action' => 'http://your server URL/Questionnaire/process_poll.php?pollTelNo='.$pollTelNo,
-		'method' => 'GET',
-		'timeout' => '30',
-		'numDigits' => '1'
-	));
-	$gather->say("Twilioアンケートへようこそ。画面に表示されている作品番号を1から3の数字ボタンをおしてください。", array('language' => 'ja-jp'));
-	
-	header("Content-type: text/xml");
-	print $response;
+  $gather = $response->gather(array(
+    'action' => 'http://your server URL/Questionnaire/process_poll.php?pollTelNo='.$pollTelNo,
+    'method' => 'GET',
+    'timeout' => '30',
+    'numDigits' => '1'
+  ));
+
+  $gather->say("Twilioアンケートへようこそ。画面に表示されている作品番号を1から3の数字ボタンをおしてください。", array('language' => 'ja-jp'));
+  header("Content-type: text/xml");
+  print $response;
 ?>
