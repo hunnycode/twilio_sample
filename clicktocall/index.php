00be55d5f592c54d52507e54af4e1afb3446a160
joohoun song <joohoun@gmail.com>
2015-09-06 15:15:44 +0900

    Update index.php

--- clicktocall/index.php
+++ clicktocall/index.php
@@ -1,24 +1,21 @@
 <?php ?>
 <htmL>
 <head>
-	<title>Twilio Sample</title>
+  <title>Twilio Sample</title>
 </head>
 <body>
-<h1>Twilio匿名通話サンプル</h1>
-<?php 
-
-if(isset($_REQUEST['msg'])) {
-	echo '<i>' . $_REQUEST['msg'] . '</i>';
-}
-
+  <h1>Twilio匿名通話サンプル</h1>
+<?php
+  if(isset($_REQUEST['msg'])) {
+    echo '<i>' . $_REQUEST['msg'] . '</i>';
+  }
 ?>
 
-<h3>転送先の電話番号を入力してください。</h3>
-<form action="makecall.php" method="post">
-	<span>発信先電話番号: <input type="text" name="to_called" /></span><br />
+  <h3>転送先の電話番号を入力してください。</h3>
+  <form action="makecall.php" method="post">
+    <span>発信先電話番号: <input type="text" name="to_called" /></span><br />
     <span>転送先電話番号: <input type="text" name="called" /></span><br />
     <input type="submit" value="Connect me!" />
-</form>
-
+  </form>
 </body>
 </html>
