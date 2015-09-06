3e612454d76303491c10fa4d816c607afe16e9df
joohoun song <joohoun@gmail.com>
2015-09-06 15:13:31 +0900

    Update twilioCall.php

--- twilio_call/twilioCall.php
+++ twilio_call/twilioCall.php
@@ -1,17 +1,17 @@
 <?php
-	require("Services/Twilio.php");
+  require("Services/Twilio.php");
 
-	$in_tel_to = substr_replace($_POST['in_tel_to'], "+81", 0,1);
-	$out_tel_to = substr_replace($_POST['out_tel_to'], "+81", 0,1);
-	$sound_list = $_POST['soundList'];
+  $in_tel_to = substr_replace($_POST['in_tel_to'], "+81", 0,1);
+  $out_tel_to = substr_replace($_POST['out_tel_to'], "+81", 0,1);
+  $sound_list = $_POST['soundList'];
 	
-	$ACCOUNT_SID = "your Twilio Account SID"; // Your Twilio account sid
-	$AUTH_TOKEN = "your Twilio Auth Token"; // Your Twilio auth token
+  $ACCOUNT_SID = "your Twilio Account SID"; // Your Twilio account sid
+  $AUTH_TOKEN = "your Twilio Auth Token"; // Your Twilio auth token
 
-	$client = new Services_Twilio($ACCOUNT_SID, $AUTH_TOKEN);
-		$call = $client->account->calls->create(
-			'your Twilio Phone Number', // From a Twilio number in your account
-			$in_tel_to, // Call any number
-			'http://your web server URL/twilio_call/Call.php?out_tel_to='.$out_tel_to.'&sound_list='.$sound_list
-		);
+  $client = new Services_Twilio($ACCOUNT_SID, $AUTH_TOKEN);
+  $call = $client->account->calls->create(
+    'your Twilio Phone Number', // From a Twilio number in your account
+    $in_tel_to, // Call any number
+    'http://your web server URL/twilio_call/Call.php?out_tel_to='.$out_tel_to.'&sound_list='.$sound_list
+  );
 ?>
