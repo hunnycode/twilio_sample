eb67c4a6c01384f87ac016ee4fadb1faefde5472
joohoun song <joohoun@gmail.com>
2015-09-06 15:36:11 +0900

    Update complete_call.php

--- callscreen/complete_call.php
+++ callscreen/complete_call.php
@@ -1,6 +1,7 @@
-<?php header("content-type: text/xml"); 
-echo '<?xml version="1.0" encoding="UTF-8?>';
+<?php 
+  header("content-type: text/xml"); 
+  echo '<?xml version="1.0" encoding="UTF-8?>';
 ?>
 <Response>
-<Say>Connecting</Say>
+  <Say>Connecting</Say>
 </Response>
